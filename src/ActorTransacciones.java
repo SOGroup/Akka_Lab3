c06d13530d87b1139882a7f5e430852b0fbaf8ae
caatovarbo <catovar360@gmail.com>
2014-04-18 22:01:58 -0500

    Akka commit

---
+++ src/ActorTransacciones.java
@@ -0,0 +1,49 @@
+
+import akka.actor.UntypedActor;
+
+public class ActorTransacciones extends UntypedActor {
+
+    private int operaciones[];
+    private String persona;
+    public static int SALDO = 0;
+    
+    public ActorTransacciones(String persona, int operaciones[]) {
+        super();
+        this.persona = persona;        
+        this.operaciones = operaciones;
+    }
+
+    @Override
+    public void onReceive(Object message) throws Exception {
+        if (message instanceof String) {
+            String s = (String) message;
+
+            switch (s) {
+                case "Operar": {
+                    for (int i = 0; i < operaciones.length; i++) {
+                        SALDO += operaciones[i];
+                        System.out.println(this.getPersona()+ i + ". Saldo despues de transaccion " + operaciones[i] + ": " + SALDO);
+                    }
+                    System.out.println("Saldo despues de operaciones de "+this.getPersona()+": "+SALDO);
+                    break;
+                }
+                default:
+                    System.out.println("huu?");
+            }
+        } else {
+            unhandled(message);
+        }
+    }
+
+    public void setOperaciones(int op[]) {
+        this.operaciones = op;
+    }
+    
+    public void setPersona(String persona) {
+        this.persona = persona;
+    }
+    
+    public String getPersona() {
+        return this.persona;
+    }
+}
---
+++ src/MainLab3.java
@@ -0,0 +1,42 @@
+
+import akka.actor.ActorContext;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class MainLab3 {
+    
+    public static void main(String[] args) throws FileNotFoundException {
+        int numA;
+        int numB;
+        
+        // Lectura archivo
+        File fichero = new File("src/Operaciones.txt");
+        Scanner sc = new Scanner(fichero);
+        numA = sc.nextInt();
+        int[] operacionesA = new int[numA]; //Operaciones A
+        numB = sc.nextInt();
+        int operacionesB[] = new int[numB]; //Operaciones B
+
+        for (int i = 0; i < numA; i++) {
+            operacionesA[i] = sc.nextInt();
+        }
+        for (int i = 0; i < numB; i++) {
+            operacionesB[i] = sc.nextInt();
+        }
+        
+        // Creación actores
+        ActorSystem system = ActorSystem.create("Banco");
+        ActorRef actorA = system.actorOf(Props.create(ActorTransacciones.class,"A",operacionesA), "A");
+        ActorRef actorB = system.actorOf(Props.create(ActorTransacciones.class,"B",operacionesB), "B");
+                
+        actorA.tell("Operar", null);
+        actorB.tell("Operar", null);
+
+        system.shutdown();
+        system.awaitTermination(); //JOIN
+    }
+}
